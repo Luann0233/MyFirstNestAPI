df34c1bbdbe658299362eb720622c10f3d5dce4a
LuanRocha <luann0233@gmail.com>
2023-02-11 15:10:23 -0300

    Criando endpoint de criar Despesas mas sem as devidas tipagens

--- src/expense/dto/create-expense.dto.ts
+++ src/expense/dto/create-expense.dto.ts
@@ -1,8 +1,18 @@
 import { Expense } from '../entities/expense.entity';
-import { IsString, MaxLength } from 'class-validator';
+import { IsNumber, IsString, MaxLength } from 'class-validator';
 
 export class CreateExpenseDto extends Expense {
   @IsString()
   @MaxLength(191)
   descricao: string;
+
+  @IsString()
+  @MaxLength(10)
+  data: string;
+
+  @IsNumber()
+  valor: number;
+
+  @IsNumber()
+  authorId: number;
 }
--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { Expense } from './entities/expense.entity';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ExpenseService {
-  create(createExpenseDto: CreateExpenseDto) {
-    return 'This action adds a new expense';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createExpenseDto: CreateExpenseDto) {
+    const data: Prisma.ExpenseCreateInput = createExpenseDto;
+
+    const createdExpense = await this.prisma.expense.create({ data });
+
+    return {
+      ...createdExpense,
+    };
   }
 
   findAll() {
