73b9fe77e0d2d24c39154267d07b04aff6a04802
LuanRocha <luann0233@gmail.com>
2023-02-11 11:10:53 -0300

    Criando estrutura de despesas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { DespesasModule } from './despesas/despesas.module';
 
 @Module({
-  imports: [PrismaModule, UsuariosModule],
+  imports: [PrismaModule, UsuariosModule, DespesasModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,7 @@
-export class Usuario {}
+export class usuarios {
+  id?: number;
+  email: string;
+  senha: string;
+  nome: string;
+  despesas: Array;
+}
