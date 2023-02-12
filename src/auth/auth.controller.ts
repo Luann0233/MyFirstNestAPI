8d58417d1d7187a63df3326b622b37eb819f5d20
LuanRocha <luann0233@gmail.com>
2023-02-12 11:38:04 -0300

    Implementacao do usuario logado

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Get,
   Request,
   UseGuards,
 } from '@nestjs/common';
@@ -10,6 +11,8 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
 import { IsPublic } from './decorators/is-public.decorator';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './../user/entities/user.entity';
 
 @Controller()
 export class AuthController {
@@ -21,5 +24,10 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   async login(@Request() req: AuthRequest) {
     return this.authService.login(req.user);
+  }
+
+  @Get('/me')
+  getMe(@CurrentUser() currentUser: User) {
+    return currentUser;
   }
 }
