03d93782a203b523803ffa68e508169628a8735a
LuanRocha <luann0233@gmail.com>
2023-02-12 11:04:45 -0300

    Implemntenado a geracao do token JWT no endpoint de login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,19 +3,23 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Request,
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
+// import { IsPublic } from './decorators/is-public.decorator';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // @IsPublic()
   @Post('login')
-  @UseGuards(LocalAuthGuard)
   @HttpCode(HttpStatus.OK)
-  login() {
-    return 'Realizar Login';
+  @UseGuards(LocalAuthGuard)
+  async login(@Request() req: AuthRequest) {
+    return this.authService.login(req.user);
   }
 }
