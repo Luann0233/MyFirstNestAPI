1efd63f9456e3e2e2fcb2af922cc762ff58f37b8
LuanRocha <luann0233@gmail.com>
2023-02-12 09:46:23 -0300

    Create Module, Controller, service atuth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ExpenseModule } from './expense/expense.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ExpenseModule],
+  imports: [PrismaModule, UserModule, ExpenseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
